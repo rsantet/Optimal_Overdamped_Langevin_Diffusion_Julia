bf795d1a69cb37d657bf2c556f7701abb18f254f
rsantet <regis.santet@hotmail.com>
2024-01-04 10:18:35 +0100

    fix function call

--- optimal_diffusion_computation.jl
+++ optimal_diffusion_computation.jl
@@ -1,25 +1,16 @@
 include("main.jl")
 include("potentials.jl")
 
-I = 500
+I = 1000
 p = 2
 a = 0.0
 b = Inf
 V = sin_two_wells
-tol = 10^(-1)
+tol = 10^(-12)
 
-function mu(x)
-    return exp(-V(x))
-end
-
-XX = [i / I for i in range(0, I)]
+XX = [i / I for i = 0:I-1]
 mu_arr = map(x -> exp(-V(x)), XX)
 Z = sum(mu_arr) / I
 pi_arr = mu_arr / Z
 
-optim_algo(
-    V, I, pi_arr, Z,
-    p=p,
-    a=a, b=b,
-    tol=tol
-);
+optim_algo(V, I, mu_arr, p = p, a = a, b = b, tol = tol);
